96c073b307a3845f90c9a8ee064bcd367dde0a96
sneha jagtap patil <sapatil2115@gmail.com>
2026-03-22 09:09:24 +0530

    update

--- app.py
+++ app.py
@@ -6,4 +6,9 @@ from tavily import TavilyClient
 
 load_dotenv()
 
+#configuration API's
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+tavily_client = TavilyClient(api_key=os.getenv("TAVILY_API_KEY"))
+
+
+print("ok")
